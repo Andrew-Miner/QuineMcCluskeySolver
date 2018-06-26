fb2cf9f695d8d9a84e4a17866880ce576d907d2c
Andrew-Miner <andrew.miner3@gmail.com>
2018-06-25 17:34:18 -0700

    Initial Commit

---
+++ Quine McCluskey Solver/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Quine McCluskey Solver
+//
+//  Created by Andrew Miner on 6/25/18.
+//  Copyright © 2018 Andrew Miner. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
